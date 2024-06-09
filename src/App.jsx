da3e7492185038bc31ebddc8c6259e5f3225f9d5
Sheik Abdul Rahman H <sheikabdulrahman2000@gmail.com>
2024-06-09 20:39:39 +0530

    Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Header from './components/Header'
+import Status from './components/Status'
 function App() {
   return (
     <>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,29 @@
 import React from 'react'
 import instaImage from '../images/instagram.png'
 const Header = () => {
-  return (
-      <div className="flex justify-between container items-center border-b">
-          <div>
-          <img src={instaImage} alt="instaImage" className='h-14' />
-          </div>
-          <div className='flex space-x-3'>
-                  <input type="text"
-                      placeholder='Search'
-                  className='px-4 h-9 w-64 rounded-lg bg-gray-200 text-slate-400 font-extralight cursor-text 
-                      focus:ring-none'
-                  />
-    
-              <div>heart</div>
-          </div>
-      </div>
+    return (
+        <div>
+            <div className='lg:block'>
+            <img src={instaImage} alt="instaImage" className='mx-4 h-14' />
+            </div>
+            <div className="lg:hidden flex justify-between container items-center border-b">
+                <div>
+                    <img src={instaImage} alt="instaImage" className='mx-4 h-14' />
+                </div>
+
+                <div className='flex space-x-3'>
+                        <input type="text"
+                            placeholder='Search'
+                            className='px-4 h-9 w-64 rounded-lg bg-gray-200 text-slate-400 font-extralight cursor-text 
+                            focus:ring-none'
+                        />
+                    <div>heart</div>
+                </div>
+            </div>
+        </div>
   )
 }
+
+
 
 export default Header
